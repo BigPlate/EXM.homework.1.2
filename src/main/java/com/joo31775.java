c2ba14cb1fefe009a903eb874e57d882781cdd29
Big_Plate <great9744@gmail.com>
2022-01-10 23:09:56 +0900

    Initial commit

---
+++ src/main/java/com/joo31775.java
@@ -0,0 +1,22 @@
+package com;
+
+import org.bukkit.World;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.GameRule;
+
+public final class joo31775 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        World world = null;
+        world.setGameRule(GameRule.KEEP_INVENTORY, false);
+
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
