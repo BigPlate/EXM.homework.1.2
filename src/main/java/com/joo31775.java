128513de4f8325fadb702c94303ac5a7475d63bf
Big_Plate <great9744@gmail.com>
2022-01-10 23:35:04 +0900

    a

--- src/main/java/com/joo31775.java
+++ src/main/java/com/joo31775.java
@@ -4,11 +4,12 @@ import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.GameRule;
 
+
 public final class joo31775 extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        World world = null;
+        World world = getServer().getWorld("a");
         world.setGameRule(GameRule.KEEP_INVENTORY, false);
 
         // Plugin startup logic
